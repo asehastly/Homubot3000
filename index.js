b1b7e8593b2789209de4a3ecbd367eaf3fb4cc94
asehastly <asehastly@gmail.com>
2019-10-18 23:40:03 +0800

    something

--- index.js
+++ index.js
@@ -57,8 +57,9 @@ bot.on('message', contri =>{
     switch(args[0]){
         case 'contribution':
             contri.delete();
+            //const img1 = new Discord.Attachment('./images/warning.png', 'warning.png')
             const disclaim = new Discord.RichEmbed()
-
+        
             .addField('This channel is being updated daily at 12:00 +8GMT and 23:00 +8GMT',"Note: Our 150 contribution quota is on a weelky basis. So don't force yourself into doing dailies if you ran out of stamina. you can always do it the next day.")
             .setColor(0x2e2e2e)
             .setImage('https://i.imgur.com/gDaxAEl.png')
@@ -72,4 +73,4 @@ bot.on('message', contri =>{
     }
 })
 
-bot.login(token);
+bot.login('NjM0NTcxMzA2MDI0ODI4OTI5.XanXuw.cQZjmW-YSysn35HWDcdMrJUNe8g');
