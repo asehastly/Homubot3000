7e01e374e365f836a8e8002cef219d3a13d5503b
asehastly <asehastly@gmail.com>
2019-10-19 00:43:58 +0800

    something

--- index.js
+++ index.js
@@ -55,20 +55,22 @@ bot.on('message', contri =>{
     let args = contri.content.substring(PREFIX.length).split(" ");
 
     switch(args[0]){
-        case 'contribution':
+        case 'con':
             contri.delete();
-            //const img1 = new Discord.Attachment('./images/warning.png', 'warning.png')
+            const img1 = new Discord.Attachment('./images/chart.png')
             const disclaim = new Discord.RichEmbed()
         
             .addField('This channel is being updated daily at 12:00 +8GMT and 23:00 +8GMT',"Note: Our 150 contribution quota is on a weelky basis. So don't force yourself into doing dailies if you ran out of stamina. you can always do it the next day.")
             .setColor(0x2e2e2e)
             .setImage('https://i.imgur.com/gDaxAEl.png')
 
-            const chart = new Discord.RichEmbed()
-            .setImage('https://i.imgur.com/gDaxAEl.png')
+            //const chart = new Discord.RichEmbed()
+            //.setImage('https://i.imgur.com/kGiFHvm.png')
 
             contri.channel.sendEmbed(disclaim);
-            contri.channel.sendEmbed(chart);
+            contri.channel.send(img1)
+            
+            //contri.channel.sendEmbed(chart);
         break;
     }
 })
