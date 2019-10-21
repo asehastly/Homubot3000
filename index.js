54c34db8f8733c45a33061d6648bea686b0d3f2e
asehastly <asehastly@gmail.com>
2019-10-21 15:47:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ require('dotenv-flow').config();
 const config = {
     token: process.env.Token,
     owner: process.env.OWNER,
-    PREFIX: process.env.PREFIX
 }
+
+const PREFIX = process.env.PREFIX;
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     bot.commands.set(command.name, command);
+}
+
+function emoji(id) {
+    return bot.emojis.get(id).toString();
 }
 
 bot.on('ready', async () => {
@@ -33,6 +38,17 @@ bot.on('ready', async () => {
 
 //#Rules Code
 
+//Member's list codes
+bot.on('message', memlist => {
+    let args = memlist.content.substring(PREFIX.length).split(' ');
+
+    if(args[0] === 'mem') {
+        bot.commands.get('Member').execute(memlist,args);
+    } else {
+        return;
+    }
+})
+
 //#Contribution Code
 bot.on('message', cont => {
     let args = cont.content.substring(PREFIX.length).split(" ");
@@ -42,6 +58,21 @@ bot.on('message', cont => {
     } else {
         return;
     }
+})
+
+// Chat Reaction
+bot.on('message', react => {
+    let args = react.content.substring(PREFIX.length).split(' ');
+
+    switch(args[0]){
+        case 'react':
+            if(!args[1]) return react.reply(`I'm not so sure how to react? ${emoji('624616931772596254')}`);
+            if(!args[2]) return react.reply(`In what room should I react too? ${emoji('624616931772596254')}`);
+            bot.commands.get('React').execute(react,args);
+        break;
+    }
+    
+
 })
 
 //end of Codes
