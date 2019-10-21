ac23b89431c66b2ea9b7d4b58ca7631b2b960f4c
asehastly <asehastly@gmail.com>
2019-10-22 00:06:18 +0800

    Update react.js

--- commands/react.js
+++ commands/react.js
@@ -4,14 +4,17 @@ module.exports = {
     execute(react, args) {
         const Discord = require('discord.js');
         const bot = new Discord.Client();
+
         function emoji(id) {
-            return bot.emojis.get(id).toString();
+            //return bot.emojis.get(id).toString();
+            return react.client.emojis.get(id).toString();
         }
 		switch(args[1]){
             case 'laugh':
                 switch(args[2]){
                     case 'gallery':
-                        bot.channels.
+                        //bot.channels.get('621572547325984769').sendMessage(`${emoji('624579235138240512')}`);
+                        react.client.channels.get('621572547325984769').sendMessage(`Funny ${emoji('624579235138240512')}`);
                     break;
                 }
             break;
