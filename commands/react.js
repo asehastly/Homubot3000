b78b7cf2b3a85aa8fa425e3417517081d70f7233
asehastly <asehastly@gmail.com>
2019-10-22 18:13:51 +0800

    Update react.js

--- commands/react.js
+++ commands/react.js
@@ -14,7 +14,11 @@ module.exports = {
                 switch(args[2]){
                     case 'gallery':
                         //bot.channels.get('621572547325984769').sendMessage(`${emoji('624579235138240512')}`);
-                        react.client.channels.get('621572547325984769').sendMessage(`Funny ${emoji('624579235138240512')}`);
+                        react.client.channels.get('621572547325984769').sendMessage(`Funny ${emoji('624579235138240512')}\n**Being Sarcastic*`);
+                    break;
+                    case 'main':
+                        //bot.channels.get('621572547325984769').sendMessage(`${emoji('624579235138240512')}`);
+                        react.client.channels.get('621536230055739408').sendMessage(`Funny ${emoji('624579235138240512')}\n**Being Sarcastic*`);
                     break;
                 }
             break;
