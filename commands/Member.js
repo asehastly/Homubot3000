1a83efa053e07b2fc88b0979b7cc571f1334e3cd
asehastly <asehastly@gmail.com>
2019-10-23 15:56:21 +0800

    Update Member.js

--- commands/Member.js
+++ commands/Member.js
@@ -9,10 +9,12 @@ module.exports = {
         const Adm = new Discord.Attachment('./images/members-list/A_Nameplate.png')
         const Vim = new Discord.Attachment('./images/members-list/V_Nameplate.png')
         const Cem = new Discord.Attachment('./images/members-list/C_Nameplate.png')
-        mem.channel.bulkDelete(6);
+        const Cem2 = new Discord.Attachment('./images/members-list/C_Nameplate-2.png')
+        mem.channel.bulkDelete(7);
         setTimeout(Admin,1000);
         setTimeout(Vice,6000);
         setTimeout(Crew,8000);
+        setTimeout(Crew2,10000);
         function Admin() {
             mem.channel.sendMessage('> Admiral');
             mem.channel.send(Adm);
@@ -24,6 +26,9 @@ module.exports = {
         function Crew() {
             mem.channel.sendMessage('> Crew Members');
             mem.channel.send(Cem);
+        }
+        function Crew2() {
+            mem.channel.send(Cem2);
         }
 	},
 };
