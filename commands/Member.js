28842f7ce834dff09458166a11c11bfc49d5b69e
asehastly <asehastly@gmail.com>
2019-10-24 00:36:58 +0800

    Update Member.js

--- commands/Member.js
+++ commands/Member.js
@@ -10,11 +10,12 @@ module.exports = {
         const Vim = new Discord.Attachment('./images/members-list/V_Nameplate.png')
         const Cem = new Discord.Attachment('./images/members-list/C_Nameplate.png')
         const Cem2 = new Discord.Attachment('./images/members-list/C_Nameplate-2.png')
-        mem.channel.bulkDelete(7);
+        mem.channel.bulkDelete(8);
         setTimeout(Admin,1000);
         setTimeout(Vice,6000);
         setTimeout(Crew,8000);
         setTimeout(Crew2,10000);
+        mem.channel.sendMessage('**HOMU is updating member list...\nPlease Stand by...').then(update => {update.delete(11000)});
         function Admin() {
             mem.channel.sendMessage('> Admiral');
             mem.channel.send(Adm);
@@ -29,6 +30,7 @@ module.exports = {
         }
         function Crew2() {
             mem.channel.send(Cem2);
+            setTimeout(Upcom => {mem.channel.sendMessage('**HOMU Update Complete**').then(update => {update.delete(3000)});},11000);
         }
 	},
 };
