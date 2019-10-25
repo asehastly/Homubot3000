a73d180a41b0ce9a7d252fe3211a03fae1553c51
asehastly <asehastly@gmail.com>
2019-10-26 00:09:40 +0800

    Update filesys.js

--- global/filesys.js
+++ global/filesys.js
@@ -1,17 +1,12 @@
 const fs = require('fs');
 module.exports = (bot) => {
     
-    const handlerFiles = fs.readdirSync('./event').filter(file => file.endsWith('.js'));
+    /*const handlerFiles = fs.readdirSync('./event').filter(file => file.endsWith('.js'));
 
     for (let file of handlerFiles) {
         const event = require(`./event/${file}`);
         bot.commands.set(event.name, event);
-    }
+    }*/
 
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-    for (let file of commandFiles) {
-        const command = require(`./commands/${file}`);
-        bot.commands.set(command.name, command);
-    }
+    
 };
