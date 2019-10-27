e106c816d36dc089fb4fa97bfbbc00d8a0bbf9d2
asehastly <asehastly@gmail.com>
2019-10-27 18:26:08 +0800

    Create guildMemberAdd.js

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,33 @@
+module.exports = (homu, member) => {
+    const moment = require('moment');
+    let welcome = member.guild.channels.find(c => c.name === 'testing-reception');
+    //let memDate = moment(moment(member.joinedAt).format("lll"));
+    var rule = '';
+    const Discord = require('discord.js')
+    const Welemb = new Discord.RichEmbed({
+        color: 0x2c2f33,
+	    title: 'Welcome Aboard Captain!',
+	    author: {
+		    name: `${member.user.tag} has joined`,
+		    icon_url: member.user.displayAvatarURL,
+        },
+        description: `**Welcome Captain ${member.displayName}** We've been waiting for you.
+        before we start, kindly visit <#621631411392086036> to familiarize yourself with our rules in our armada. then, head over to <#623773208667095070> and pick the color you like.`,
+        thumbnail: {
+            url: 'https://i.imgur.com/JzDnCGJ.png',
+        },
+        fields: [
+            {
+                name: '⠀',
+                value: 'Have a great day and #JoinTheConversation at <#621536230055739408>',
+            },
+        ],
+        footer: {
+            text: `Joined at: ${member.joinedAt}`,
+            icon_url: member.guild.iconURL,
+        },
+    })
+
+    welcome.sendEmbed(Welemb);
+
+}
