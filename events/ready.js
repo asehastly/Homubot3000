57dff14d723a2cdd51340409a2734705064bc585
asehastly <asehastly@gmail.com>
2019-10-27 18:26:16 +0800

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,9 @@
+module.exports = homu => {
+    console.log(`${homu.user.tag} logging in...`);
+    setTimeout(homuRun => {
+        console.log('HomuBot3000 logged and ready to go!');
+    },7000)
+    setTimeout(homuRun => {
+        console.log(' ');
+    },8000)
+}
