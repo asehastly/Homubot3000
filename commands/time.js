4f5ecd12beb9170a0c3a1b792a9e0551810c96e7
asehastly <asehastly@gmail.com>
2019-10-29 01:06:09 +0800

    Update time.js

--- commands/time.js
+++ commands/time.js
@@ -1,10 +1,10 @@
 exports.run = (homu, message, args) => {
 	var moment = require('moment')
     var startDate = moment()
-    var endDate = moment('00:50:00', 'HH:mm:ss')
+    var endDate = moment('01:25:00', 'HH:mm:ss')
     var calcTime = endDate.diff(startDate, 'miliseconds')
 
-    msg.channel.sendMessage('Going to send a message in 8:00 PM.')
+    msg.channel.sendMessage('Going to send a message in 1:25 AM.')
     setTimeout(delay,calcTime);
     function delay() {
         msg.channel.sendMessage("Time's up!")
