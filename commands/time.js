c7e2a89116b49d3844965bb554a6efc1d313233f
asehastly <asehastly@gmail.com>
2019-10-29 00:35:16 +0800

    Update time.js

--- commands/time.js
+++ commands/time.js
@@ -1,16 +1,18 @@
-module.exports = {
-	name: 'time',
-	description: 'testing command for setTimeOut command',
-	execute(msg, args) {
-		var moment = require('moment')
-        var startDate = moment()
-        var endDate = moment('22:25:00', 'HH:mm:ss')
-        var calcTime = endDate.diff(startDate, 'miliseconds')
+exports.run = (homu, message, args) => {
+	var moment = require('moment')
+    var startDate = moment()
+    var endDate = moment('00:50:00', 'HH:mm:ss')
+    var calcTime = endDate.diff(startDate, 'miliseconds')
 
-        msg.channel.sendMessage('Going to send a message in 8:00 PM.')
-        setTimeout(delay,calcTime);
-        function delay() {
-            msg.channel.sendMessage("Time's up!")
-        }
-	},
+    msg.channel.sendMessage('Going to send a message in 8:00 PM.')
+    setTimeout(delay,calcTime);
+    function delay() {
+        msg.channel.sendMessage("Time's up!")
+    }
 };
+
+exports.help = {
+	name: 'time'
+}
+
+
