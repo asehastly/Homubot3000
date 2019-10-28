6b2d5426e13e4157c8ff692710dfd2936e4e6584
asehastly <asehastly@gmail.com>
2019-10-28 22:24:21 +0800

    Create setboss.js

---
+++ commands/setboss.js
@@ -0,0 +1,15 @@
+exports.run = (homu, message, args) => {
+    const Discord = require('discord.js');
+    
+    const stbs = args;
+
+    if(stbs[0] === 'yae' && stbs[1] === 'sakura') {
+        var boss = 'Yae Sakura'
+        var lvl = stbs[3];
+    } 
+    
+};
+
+exports.help = {
+	name: 'setboss'
+}
