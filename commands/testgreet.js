c527cfb5f0a48aa6959f5c26ecf5631f9dd6c84e
asehastly <asehastly@gmail.com>
2019-10-29 00:35:12 +0800

    Update testgreet.js

--- commands/testgreet.js
+++ commands/testgreet.js
@@ -1,6 +1,6 @@
 exports.run = async (homu, message, args) => {
-    let testG = homu.emit('guildMemberAdd', message.member);
-    message.channel.send(testG);
+    homu.emit('guildMemberAdd', message.member);
+    //message.channel.send(testG);
 }
 exports.help = {
     name: 'testGreet'
