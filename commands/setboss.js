639b8fafe231d10455f1648395e37c321a9861dd
asehastly <asehastly@gmail.com>
2019-10-28 22:42:49 +0800

    Update setboss.js

--- commands/setboss.js
+++ commands/setboss.js
@@ -2,11 +2,18 @@ exports.run = (homu, message, args) => {
     const Discord = require('discord.js');
     
     const stbs = args;
+    var boss
+    var lvl
 
     if(stbs[0] === 'yae' && stbs[1] === 'sakura') {
-        var boss = 'Yae Sakura'
-        var lvl = stbs[3];
-    } 
+        boss = 'Yae Sakura'
+        lvl = stbs[2];
+    } else {
+        boss = stbs[0].charAt(0).toUpperCase() + stbs[0].substring(1);
+        lvl = stbs[1];
+    }
+
+    message.channel.send(`Sample:\nBoss Invasion is set!\n**${boss}**\nat level ${lvl}`);
     
 };
 
