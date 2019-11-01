ca208c37f0414cfdd6d13a19d365b3d579e966ef
asehastly <asehastly@gmail.com>
2019-11-01 16:54:56 +0800

    Create currenttime.js

---
+++ commands/currenttime.js
@@ -0,0 +1,27 @@
+exports.run = (homu, message, args) => {
+	var moment = require('moment');
+    var startDate = moment();
+    var f_noti = moment('16:00:00', 'HH:mm:ss');
+    var s_noti = moment('17:00:00', 'HH:mm:ss');
+    //var f_display = moment('11:40:00', 'HH:mm:ss');
+    var calc_f = f_noti.diff(startDate, 'miliseconds');
+    var calc_s = s_noti.diff(startDate, 'miliseconds');
+
+    message.channel.send(`Going to send a message in ${f_noti.format('hh:mm A')} and ${s_noti.format('hh:mm A')}`);
+    console.log(`time.js engaged:\nStart time Initiated:\n${startDate.format('hh:mm A')}\nTime Stop initiated:\n${f_noti.format('hh:mm A')} folowed by ${s_noti.format('hh:mm A')}\nTime Elapsed in mil.:\n${calc_f} followed by ${calc_s}`)
+    //console.log(calcTime);
+    /* setTimeout(delay1 => {
+        message.channel.send("<@&621558338005630978> 1st Time's up!");
+        console.log('Timer 1 stopped.');
+    },calc_f);
+    setTimeout(delay2 => {
+        message.channel.send("<@&621558338005630978> 2nd Time's up!");
+        console.log('Timer 2 stopped.');
+    },calc_s); */
+};
+
+exports.help = {
+	name: 'currenttime'
+}
+
+
