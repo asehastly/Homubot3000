8667e89503c6bfb4f225ef95185b407b49a6e23c
asehastly <asehastly@gmail.com>
2019-11-03 17:24:44 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,33 +1,35 @@
 exports.run = (homu, message, args) => {
     const Discord = require('discord.js');
     const moment = require('moment-timezone');
+    var date = moment(message.author.createdAt).format('ddd DDD MMM, YYYY h:mm A');
+    var zone =  moment.tz.guess();
+    //var abb = moment.tz(zone_name).zoneAbbr()
 
-    var hour = moment().tz('Asia/Manila').format('HH');
-	var startDate = moment().tz('Asia/Manila').format('HH:mm:ss');
-	var f_noti = moment('9:00:00', 'HH:mm:ss').tz('Asia/Manila').format('HH:mm:ss');
-    var s_noti = moment('10:00:00', 'HH:mm:ss').tz('Asia/Manila').format('HH:mm:ss');
-	//Computation from the time boss was set to 5:00 PM
-    var mf = moment(f_noti,"HH:mm:ss").diff(moment(startDate,"HH:mm:ss"));
-    var hf = moment.duration(mf);
-	//Computation from the time boss was set to 6:00 PM
-    var ms = moment(s_noti,"HH:mm:ss").diff(moment(startDate,"HH:mm:ss"));
-    var hs = moment.duration(ms);
-	
-	//Computed number of milliseconds for the first post
-    var calc_f = Math.floor(hf.asHours()) + moment.utc(mf)
-    var display_f= Math.floor(hf.asHours()) + moment.utc(mf).format(":mm:ss");
-	//Computed number of milliseconds for the second post
-    var calc_s = Math.floor(hs.asHours()) + moment.utc(ms)
-    var display_s= Math.floor(hs.asHours()) + moment.utc(ms).format(":mm:ss");
-
-    message.channel.send(`${hour}\n${startDate}\n${f_noti}\n${s_noti}`);
-	message.channel.send(`${display_f}\n${display_s}`);
-    // between 6 PM and 11 pm respectively
-    if (hour >= 0 && hour <= 17) {
-        message.channel.send("Wow! you can access this command now.\nGood on ya'");
-    } else {
-        message.channel.send('Boss Invasion is still on going.\nYou can not set the boss yet.');
-    }
+    const Welemb = new Discord.RichEmbed({
+        color: 0x2c2f33,
+	    title: 'Welcome Aboard Captain!',
+	    author: {
+		    name: `${homu.user.tag} has joined`,
+		    icon_url: homu.user.displayAvatarURL,
+        },
+        description: `**Welcome Captain ${message.displayName}** We've been waiting for you.
+        before we start, kindly visit <#621631411392086036> to familiarize yourself with our rules in our armada. then, head over to <#623773208667095070> and pick the color you like.`,
+        thumbnail: {
+            url: 'https://i.imgur.com/JzDnCGJ.png',
+        },
+        fields: [
+            {
+                name: '⠀',
+                value: 'Have a great day and #JoinTheConversation at <#621536230055739408>',
+            },
+        ],
+        //timestamp: new Date(),
+        footer: {
+            text: `${message.guild}™ Armada\n${date} ${zone}`,
+            icon_url: message.guild.iconUrl,
+        },
+    })
+    message.channel.send(Welemb);
 };
 
 exports.help = {
