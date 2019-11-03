59e4d11241a84ab821ba9cc1105690de0129a5d6
asehastly <asehastly@gmail.com>
2019-11-03 21:37:31 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,37 +1,22 @@
 exports.run = (homu, message, args) => {
     const Discord = require('discord.js');
-    const moment = require('moment-timezone');
-    var date = moment(message.author.createdAt).format('ddd DDD MMM, YYYY h:mm A');
-    var zone =  moment.tz.guess();
-    //var abb = moment.tz(zone_name).zoneAbbr()
 
-    const Welemb = new Discord.RichEmbed({
-        color: 0x2c2f33,
-	    title: 'Welcome Aboard Captain!',
-	    author: {
-		    name: `${homu.user.tag} has joined`,
-		    icon_url: homu.user.displayAvatarURL,
-        },
-        description: `**Welcome Captain ${message.displayName}** We've been waiting for you.
-        before we start, kindly visit <#621631411392086036> to familiarize yourself with our rules in our armada. then, head over to <#623773208667095070> and pick the color you like.`,
-        thumbnail: {
-            url: 'https://i.imgur.com/JzDnCGJ.png',
-        },
-        fields: [
-            {
-                name: '⠀',
-                value: 'Have a great day and #JoinTheConversation at <#621536230055739408>',
-            },
-        ],
-        //timestamp: new Date(),
-        footer: {
-            text: `${message.guild}™ Armada\n${date} ${zone}`,
-            icon_url: message.guild.iconUrl,
-        },
-    })
-    message.channel.send(Welemb);
+    message.delete();
+    const policy = new Discord.RichEmbed()
+            .setColor(0x2c2f33)
+            .setTitle('If you recieved a warning or a strike, what can you do?')
+            .setAuthor('Rules|Our Warning and Strike Procedure', 'https://i.imgur.com/QrJKwNl.png', ' ')
+            .setDescription(`Here are some of the things you can do for your warning or strikes to be lifted.`)
+            .addBlankField()
+            .addField('If you recieved a warning', `Simply DM, mention, or whisper me on discord or on the game as to why you can't or wasn't able to do it. thusly we can lift your warning as soon as possible.`)
+            .addField('If you recieved your 1st Strike', "Giving any reason at this point won't do. but we can still lift you strike if you can complete **150 Contribution** or more the following week.")
+            .addField('If you recieved your 2st Strike', "Your strike will only be lifted if you complete at least **300 Contribution** or more by the following week.")
+            .addField('If you recieved your 3st Strike', "This will be your last chance to complete **450 Contribution** or more the following week")
+            .addField('If you failed to complete your 3rd strike', "Will result in the termination of your membership to our armada.")
+            .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
+    message.channel.send(policy);
 };
 
 exports.help = {
-	name: 'test'
+	name: 'policy'
 }
