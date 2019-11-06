4ede3d8ba3b1783d223f380621847c0f88e42af7
asehastly <asehastly@gmail.com>
2019-11-06 18:05:22 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,19 @@
+const fs = module.require('fs');
+
 exports.run = (homu, message, args) => {
     const Discord = require('discord.js');
     const moment = require('moment-timezone');
+    
+    if(!args[0]) return message.channel.send('Please mention someone');
 
     let user =  message.mentions.users.first();
     let gmem = message.mentions.members.first();
     let DateJoin = moment(gmem.joinedAt).format('ddd DD MMM, YYYY | h:mm A');
     let DateCreate = moment(user.createdAt).format('ddd DD MMM, YYYY | h:mm A');
     let avt = user.displayAvatarURL
+    //const role = gmem.roles.find("name", "admiral");
     
+    console.log(user.id);
     const memEMB = new Discord.RichEmbed()
             .setColor(0x2c2f33)
             .setAuthor(`${message.guild.name} Membership Card`, 'https://i.imgur.com/1K2pdQH.png', ' ')
@@ -21,27 +27,30 @@ exports.run = (homu, message, args) => {
             .addField('Game ID:', "XXXXXXXX", true)
             .addField('In-Game Name:', "anonymous", true)
             //.addField('[Insert detail name here]:', "[insert detail here]", true)
-            //.addField('If you recieved your 2st Strike', "Your strike will only be lifted if you complete at least **300 Contribution** or more by the following week.")
-            //.addField('If you recieved your 3st Strike', "This will be your last chance to complete **450 Contribution** or more the following week")
-            //.addField('If you failed to complete your 3rd strike', "Will result in the termination of your membership to our armada.")
             .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
         
         //message.delete();
-        message.channel.send(memEMB);
+        homu.channels.get('621559026228133908').sendEmbed(memEMB);
+    
+    if(message.member.roles.find("name", "Vice Admirals") || message.member.roles.find("name", "Admiral")){
+        message.channel.send("Yo' Admin");// Rest of your code
+    }else{
+        message.channel.send(' was expecting an admin would use this...');// not vip
+    }
 
-        function getUserFromMention(mention) {
-            if (!mention) return;
+    function getUserFromMention(mention) {
+        if (!mention) return;
         
-            if (mention.startsWith('<@') && mention.endsWith('>')) {
-                mention = mention.slice(2, -1);
+        if (mention.startsWith('<@') && mention.endsWith('>')) {
+            mention = mention.slice(2, -1);
         
-                if (mention.startsWith('!')) {
-                    mention = mention.slice(1);
-                }
-        
-                return homu.users.get(mention);
+            if (mention.startsWith('!')) {
+                mention = mention.slice(1);
             }
+        
+            return homu.users.get(mention);
         }
+    }
 };
 
 exports.help = {
