333cea1de8e1ae8550993f5c5c555655d20b9dcb
asehastly <asehastly@gmail.com>
2019-11-07 00:15:21 +0800

    Update setboss.js

--- commands/setboss.js
+++ commands/setboss.js
@@ -1,9 +1,10 @@
+const fs = module.require('fs');
+
 exports.run = (homu, message, args) => {
     const Discord = require('discord.js');
     const moment = require('moment-timezone');
 
     var hour = moment().tz('Asia/Manila').format('HH');
-
     var local = moment();
     var startDate = moment().tz('Asia/Manila').format('HH:mm:ss');
     var f_noti = moment('9:00:00', 'HH:mm:ss').tz('Asia/Manila').format('HH:mm:ss');
@@ -14,14 +15,12 @@ exports.run = (homu, message, args) => {
     //Computation from the time boss was set to 6:00 PM
     var ms = moment(s_noti, "HH:mm:ss").diff(moment(startDate, "HH:mm:ss"));
     var hs = moment.duration(ms);
-
     //Computed number of milliseconds for the first post
     var calc_f = Math.floor(hf.asHours()) + moment.utc(mf)
     var display_f = Math.floor(hf.asHours()) + moment.utc(mf).format(":mm:ss");
     //Computed number of milliseconds for the second post
     var calc_s = Math.floor(hs.asHours()) + moment.utc(ms)
     var display_s = Math.floor(hs.asHours()) + moment.utc(ms).format(":mm:ss");
-
     // between 6 PM and 11 pm respectively
     if (hour >= 0 && hour <= 17) {
 
@@ -61,7 +60,16 @@ exports.run = (homu, message, args) => {
                 console.log(`${boss}\nCondition 4 selected`);
             }
 
-            const bossEmb = new Discord.RichEmbed()
+            homu.boss['current'] = {
+                name: boss,
+                level: lvl,
+                image: bsimg,
+                status: condition
+            }
+
+            fs.writeFile('./json/boss.json', JSON.stringify(homu.boss, null, 4), err => {
+                if(err) throw err;
+                const bossEmb = new Discord.RichEmbed()
                 .setColor(0x2c2f33)
                 .setTitle('Next Boss has been set up!')
                 .setAuthor('Boss Invasion Alert!', 'https://i.imgur.com/QrJKwNl.png', ' ')
@@ -72,51 +80,19 @@ exports.run = (homu, message, args) => {
                 .setImage(bsimg)
                 .setTimestamp()
                 .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
-            switch (condition) {
-                case '1':
-                    message.channel.sendEmbed(bossEmb);
-                    //Fist post trigger
-                    setTimeout(delay1 => {
-                        const firstEmb = new Discord.RichEmbed()
-                            .setColor(0xcccc00)
-                            .setTitle('Next Boss has been set up!')
-                            .setAuthor('Boss Invasion Alert!', 'https://i.imgur.com/QrJKwNl.png', ' ')
-                            .setDescription(`**${boss}**\nLevel ${lvl}\nGame Starts in 1 hour`)
-                            .setThumbnail('https://i.imgur.com/JzDnCGJ.png')
-                            .setImage(bsimg)
-                            .setTimestamp()
-                            .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
-
-                        message.channel.send("<@&621558338005630978> Get ready!!!");
-                        homu.channels.get('621547605138341898').send("<@&621558338005630978> Get ready!!!");
-                        homu.channels.get('621547605138341898').sendEmbed(firstEmb);
-                    }, calc_f);
-                    //Second post trigger
-                    setTimeout(delay2 => {
-                        const SecEmb = new Discord.RichEmbed()
-                            .setColor(0xb20000)
-                            .setTitle('Next Boss has been set up!')
-                            .setAuthor('Boss Invasion Alert!', 'https://i.imgur.com/QrJKwNl.png', ' ')
-                            .setDescription(`**${boss}**\nLevel ${lvl}\nBoss Invasion Starts Now\nGood luck!`)
-                            .setThumbnail('https://i.imgur.com/JzDnCGJ.png')
-                            .setImage(bsimg)
-                            .setTimestamp()
-                            .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
-
-                            homu.channels.get('621547605138341898').send("<@&621558338005630978> It's Go time!");
-                        homu.channels.get('621547605138341898').sendEmbed(SecEmb);
-                    }, calc_s);
-
+                switch (condition) {
+                    case '1':
+                        message.channel.sendEmbed(bossEmb);
                     return;
-                default:
-                    message.channel.send('Boss record not found. please try again...');
+                    default:
+                        message.channel.send('Boss record not found. please try again...');
                     return;
-            }
+                }
+            })
+
         } else {
             message.channel.send('Only Authorized personnel can use this command.\nCome back later, if you have permission...');// not vip
         }
-
-
 
     } else {
         message.channel.send('Boss Invasion is still on going.\nYou can not set the boss yet.');
