8263127395507a2d6368b9617e8022b028c4506e
asehastly <asehastly@gmail.com>
2019-11-06 18:03:29 +0800

    Update canvas.js

--- commands/canvas.js
+++ commands/canvas.js
@@ -1,9 +1,10 @@
+const fs = module.require('fs');
+
 exports.run = (homu, message, args) => {
 	const input = args;
-
+o
 	if (input[0] === 'student') {
 		const fs = require('fs');
-		homu.jvaule = require('./json/student.json');
 
 		fs.readFile('./json/student.json', (err, data) => {
 			if (err) throw err;
@@ -11,7 +12,7 @@ exports.run = (homu, message, args) => {
 			console.log(student);
 		});
 		console.log('This is after the read call');
-		message.channel.send(`Name: ${student[name]}\nAge: ${student[age]}`);
+		//message.channel.send(`Name: ${student[name]}\nAge: ${student[age]}`);
 	} else {
 		return;
 	}
