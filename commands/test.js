406a9269dd15893c492ec3fb51d35ccc4ec2cbad
asehastly <asehastly@gmail.com>
2019-11-08 01:45:20 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,18 +2,29 @@ const discord = module.require('discord.js');
 const fs = module.require('fs');
 
 exports.run = (homu, message, args) => {
-	const name = args[0];
-	const id = args[1];
+	
+	var name,id;
+	const i = args[1];
+
+	if(typeof i == 'number') {
+		name = args[0];
+		id = args[1];
+		console.log('statement 1')
+	} else {
+		name = `${args[0]} ${args[1]}`;
+		id = args[2];
+		console.log('Else statement')
+	}
 	const user = message.author.id;
 	const vnum = /^[0-9]+$/;
 
 	if(!name && !id) return message.channel.send('To register, Please enter your in-game name and game id by\ntyping homu register <in-game name> <game id>');
 
-	console.log(`User has input:\n${name} and ${id}\n${id.length}`)
+	console.log(`User has input:\n${name} and ${id}\nValue of i is ${i}\n\n${args}`)
 	if(typeof parseInt(id) == 'number' && id.length == 8) {
-		message.channel.send(`${id} is a number`);
+		message.channel.send(`IGN: **${name}**\nGame ID:${id}\nThanks for registering your details.`);
 	} else {
-		message.channel.send('you must enter your game ID properly');
+		message.channel.send('invalid ID number.\nID must contain 8 numbers.');
 	}
 	/* homu.jmem[user] = {
 		name: input[0],
