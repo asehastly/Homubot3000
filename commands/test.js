30bb04f9b8aa33e73416c6f2ed1f7eb69c4eb577
asehastly <asehastly@gmail.com>
2019-11-09 13:42:07 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,8 +3,22 @@ const fs = module.require('fs');
 
 exports.run = (homu, message, args) => {
 	const server = message.guild.id;
+	var userid = message.author.id;
+	var name = args[0];
+	var id = args[1];
 
-	message.channel.send(server);
+	homu.jmem[server] = {
+		user: userid,
+		info: {
+			ign: name,
+			gid: id
+		}
+	}
+
+	fs.writeFile('./json/member.json', JSON.stringify(homu.jmem, null, 4), err => {
+		if(err) throw err;
+		message.channel.send(`IGN: **${name}**\nGame ID:${id}\nThanks for registering your details.`);
+	})
 };
 
 exports.help = {
