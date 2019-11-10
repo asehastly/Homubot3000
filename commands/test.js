f183d32740b057833aff0871655f6f75721e68ed
asehastly <asehastly@gmail.com>
2019-11-10 18:21:34 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,24 +1,12 @@
 const discord = module.require('discord.js');
 const fs = module.require('fs');
+//const SQLite = module.require("better-sqlite3");
 
 exports.run = (homu, message, args) => {
-	const server = message.guild.id;
-	var userid = message.author.id;
-	var name = args[0];
-	var id = args[1];
+	if (message.author.bot) return;
+	let mChan = message.mentions.channels.first().id;
 
-	homu.jmem[server] = {
-		user: userid,
-		info: {
-			ign: name,
-			gid: id
-		}
-	}
-
-	fs.writeFile('./json/member.json', JSON.stringify(homu.jmem, null, 4), err => {
-		if(err) throw err;
-		message.channel.send(`IGN: **${name}**\nGame ID:${id}\nThanks for registering your details.`);
-	})
+	message.channel.send(mChan);
 };
 
 exports.help = {
