f3e1f13862789fbb04863a243086528e6a85d458
asehastly <asehastly@gmail.com>
2019-11-12 17:57:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const Enmap = require('enmap');
-//const SQLite = require("better-sqlite3");
-//const sqmem = new SQLite('./member.sqlite');
+const mysql = require('mysql');
 const homu = new Discord.Client();
 homu.collection = new Discord.Collection();
 homu.commands = new Enmap();
@@ -13,39 +12,10 @@ homu.boss = require('./json/boss.json');
 homu.config = require('./json/config.json');
 const { token } = require('./config.js');
 
-/* const config = {
-    token: process.env.TOKEN,
-    owner: process.env.OWNER,
-    prefix: process.env.PREFIX
-}
-const prefix = config.prefix;
-
-homu.on('ready', () => {
-    console.log(`${homu.user.tag} logging in...`);
-    setTimeout(homuRun => {
-        console.log('HomuBot3000 logged and ready to go!');
-    },7000)
-    setTimeout(homuRun => {
-        console.log(' ');
-    },8000)
-})
 /*Discord Bot command array sample,
 h!mute @user 12h Posting too many shit memes
 [0][1]  [2]  [3]  [4]    [5] [6]   [7]  [8]
 h!mute <user> <time> <reason>               */
-
-/* homu.on('message', message => {
-    if(message.author.bot) return;
-    if(message.content.indexOf(prefix)!== 0) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    const cmd = homu.commands.get(command);
-    if(!cmd) return;
-
-    cmd.run(homu, message, args);
-
-}); */
 
 fs.readdir('./events/', (err, files) => {
     if(err) return console.error;
