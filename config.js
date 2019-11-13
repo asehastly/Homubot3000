928ba9dd3244b61f7a78572d580fe4c3df1e4b2e
asehastly <asehastly@gmail.com>
2019-11-13 15:22:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,22 @@
 require('dotenv-flow').config();
+const mysql = require('mysql');
+
+let con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: 'homu'
+});
+
+con.connect(err => {
+    if(err) throw err;
+    console.log('connected to homu database');
+    //con.query('SHOW TABLES', console.log);
+});
+
 module.exports = {
     token: process.env.TOKEN,
     owner: process.env.OWNER,
-    prefix: process.env.PREFIX
+    prefix: process.env.PREFIX,
+    con
 }
