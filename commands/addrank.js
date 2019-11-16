8b07d3a82629d51bf99bd119da9d758f0710563a
asehastly <asehastly@gmail.com>
2019-11-17 00:09:37 +0800

    Create addrank.js

---
+++ commands/addrank.js
@@ -0,0 +1,43 @@
+const Discord = module.require('discord.js');
+const mysql = module.require('mysql');
+const { con } = require('../config.js');
+
+exports.run = (homu, message, args) => {
+    const input = args;
+    let name, dmg;
+    if(input.length <= 0) {
+        message.channel.send("where's the name and score, dude?");
+    } else if(input.length === 3) {
+        name = `${args[0]} ${args[1]}`;
+        dmg = args[2];
+        message.channel.send(`Array length: ${input.length}\nname: **${name}**\nDamage score: ${dmg}`);
+
+       addUser();
+    } else {
+        name = args[0];
+        dmg = args[1];
+        message.channel.send(`Array length: ${input.length}\nname: **${name}**\nDamage score: ${dmg}`);
+
+        addUser();
+    }
+
+    function addUser() {
+        con.query(`SELECT * FROM birank WHERE name = '${name}'`, (err, rows) => {
+            if(err) throw err;
+            
+            let sql;
+            if(rows.length < 1) {
+                sql = `INSERT INTO birank (name, dmg) VALUES ('${name}', '${parseInt(dmg)}')`;
+            } else {
+                let boss = rows[0].dmg;
+                sql = `UPDATE boss SET dmg = '${dmg}' WHERE name = '${name}'`;
+            }
+
+            con.query(sql,console.log);
+        })
+    }
+};
+
+exports.help = {
+	name: 'addrank'
+}
