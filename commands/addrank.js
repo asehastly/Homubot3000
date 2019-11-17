65ae15347f9fe1c7fe1b1ef0903dbdbf65622759
asehastly <asehastly@gmail.com>
2019-11-18 00:41:52 +0800

    Update addrank.js

--- commands/addrank.js
+++ commands/addrank.js
@@ -10,15 +10,18 @@ exports.run = (homu, message, args) => {
     } else if(input.length === 3) {
         name = `${args[0]} ${args[1]}`;
         dmg = args[2];
-        message.channel.send(`Array length: ${input.length}\nname: **${name}**\nDamage score: ${dmg}`);
-
-       addUser();
+        message.delete();
+        if (message.member.roles.find("name", "Vice Admirals") || message.member.roles.find("name", "Admiral")) {
+            addUser();
+        } else return message.channel.send("You don't have permission to use this command. go away!").then(update => { update.delete(5000) });
+       
     } else {
         name = args[0];
         dmg = args[1];
-        message.channel.send(`Array length: ${input.length}\nname: **${name}**\nDamage score: ${dmg}`);
-
-        addUser();
+        message.delete();
+        if (message.member.roles.find("name", "Vice Admirals") || message.member.roles.find("name", "Admiral")) {
+            addUser();
+        } else return message.channel.send("You don't have permission to use this command. go away!").then(update => { update.delete(5000) });
     }
 
     function addUser() {
@@ -28,9 +31,11 @@ exports.run = (homu, message, args) => {
             let sql;
             if(rows.length < 1) {
                 sql = `INSERT INTO birank (name, dmg) VALUES ('${name}', '${parseInt(dmg)}')`;
+                message.channel.send(`**${name}** had been added to the list\nwith a score of ${dmg}.`).then(update => { update.delete(5000) });
             } else {
                 let boss = rows[0].dmg;
-                sql = `UPDATE boss SET dmg = '${dmg}' WHERE name = '${name}'`;
+                sql = `UPDATE birank SET dmg = '${dmg}' WHERE name = '${name}'`;
+                message.channel.send(`**${name}** had been updated to the list\nwith a score of ${dmg}.`).then(update => { update.delete(5000) });
             }
 
             con.query(sql,console.log);
