bf743bcac8825dfbb0c6ddd5b5d406d2bec79420
asehastly <asehastly@gmail.com>
2019-11-20 00:42:05 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,59 +1,45 @@
 const Discord = module.require('discord.js');
-const fs = module.require('fs');
+const { con } = require('../config.js');
 const biconfig = require('../global/bitimer.js');
 
 module.exports = homu => {
     console.log(`${homu.user.tag} logging in...`);
-    setTimeout(homuRun => {console.log(`${homu.user.tag} logged and ready to go!\n`);},7000)
-    
+    setTimeout(homuRun => {console.log(`${homu.user.tag} logged and ready to go!\n`);},7000);
+    var stat;
+    if (hour >= 0 && hour <= 17) {
+        let id = message.guild.id;
+        con.query(`SELECT * FROM boss WHERE id = ${id}`, (err, row) => {
+            if (err) throw err;
+            stat = row[0].stat;
+            if(!stat) {
+                console.log('*WARNING*\nDatbase is empty!');
+            }else if(parseInt(stat) === 1) {
+                //if Boss name exist
+                console.log(`Boss ${row[0].name} has been set!`);
+            } else {
+                console.log('No boss was set');
+            };
+        });
+    } else {
+        console.log('Boss Invasion is still on-going');
+    };
     homu.setInterval(() => {
-        if(parseInt(stat) === 1) {
-             //Fist post trigger
-            console.log(`BI data found. Now setting variables...\nBoss: ${boss} Level: ${lvl}\nImage URL: ${bsimg}\nnow setting up setTimeouts\nFirst: ${display_f}\nSecond ${display_s}`);
-            setTimeout(delay1 => {
-                const firstEmb = new Discord.RichEmbed()
-                    .setColor(0xcccc00)
-                    .setTitle('Next Boss has been set up!')
-                    .setAuthor('Boss Invasion Alert!', 'https://i.imgur.com/QrJKwNl.png', ' ')
-                    .setDescription(`**${boss}**\nLevel ${lvl}\nGame Starts in 1 hour`)
-                    .setThumbnail('https://i.imgur.com/JzDnCGJ.png')
-                    .setImage(bsimg)
-                    .setTimestamp()
-                    .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
-
-                //message.channel.send("<@&621558338005630978> Get ready!!!");
-				console.log('First timer execute');
-                homu.channels.get('621547605138341898').send("<@&621558338005630978> Get ready!!!");
-                homu.channels.get('621547605138341898').sendEmbed(firstEmb);
-            }, biconfig.calc1());
-            //Second post trigger
-            setTimeout(delay2 => {
-                const SecEmb = new Discord.RichEmbed()
-                    .setColor(0xb20000)
-                    .setTitle('Next Boss has been set up!')
-                    .setAuthor('Boss Invasion Alert!', 'https://i.imgur.com/QrJKwNl.png', ' ')
-                    .setDescription(`**${boss}**\nLevel ${lvl}\nBoss Invasion Starts Now\nGood luck!`)
-                    .setThumbnail('https://i.imgur.com/JzDnCGJ.png')
-                    .setImage(bsimg)
-                    .setTimestamp()
-                    .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
-					
-                console.log('Second timer execute');
-                homu.channels.get('621547605138341898').send("<@&621558338005630978> It's Go time!");
-                homu.channels.get('621547605138341898').sendEmbed(SecEmb);
-				console.log('prepairing to delete boss.json entires');
-                delete homu.boss[server];
-                
-                fs.writeFile('./json/boss.json', JSON.stringify(homu.boss), err => {
-                    if(err) throw err;
-                    stat = 0;
-                    if (!homu.jmem.hasOwnProperty(user.id))
-                    console.log(`BI Procedure complete.\nboss.jason has been cleared.\nNow waiting for data in the next 12 hours.\nHave a good day.\nstat value: ${stat}`);
-                })
-            }, biconfig.calc2());
+        if (hour >= 0 && hour <= 17) {
+            let id = message.guild.id;
+            con.query(`SELECT * FROM boss WHERE id = ${id}`, (err, row) => {
+                if (err) throw err;
+                stat = row[0].stat;
+                if(!stat) {
+                    console.log('*WARNING*\nDatbase is empty!');
+                }else if(parseInt(stat) === 1) {
+                    //if Boss name exist
+                    console.log(`Boss ${row[0].name} has been set!`);
+                } else {
+                    console.log('No boss was set');
+                };
+            });
         } else {
-            return;
-        }
-               
+            console.log('Boss Invasion is still on-going');
+        };
     }, 43200000);
 }
