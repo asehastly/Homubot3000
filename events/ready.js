17a55d8bd8b1730ae60c2e4eaa298fb559dc9ba3
asehastly <asehastly@gmail.com>
2019-11-21 09:36:36 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,15 @@
 const Discord = module.require('discord.js');
+const moment = module.require('moment-timezone');
 const { con } = require('../config.js');
 const biconfig = require('../global/bitimer.js');
 
 module.exports = homu => {
     console.log(`${homu.user.tag} logging in...`);
     setTimeout(homuRun => {console.log(`${homu.user.tag} logged and ready to go!\n`);},7000);
+    var hour = moment().tz('Asia/Manila').format('HH');
     var stat;
     if (hour >= 0 && hour <= 17) {
-        let id = message.guild.id;
+        let id = homu.member.guild.id;
         con.query(`SELECT * FROM boss WHERE id = ${id}`, (err, row) => {
             if (err) throw err;
             stat = row[0].stat;
