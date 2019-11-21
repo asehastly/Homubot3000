0884b91e031c7af959f56b7d6c6bd4f5c4b3c9be
asehastly <asehastly@gmail.com>
2019-11-21 16:33:25 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -12,29 +12,48 @@ exports.run = (homu, message, args) => {
     var lvl = 55;
     var bsimg = 'https://i.imgur.com/tzxM9XE.png';
     var stat = 1;
+    var current = moment();
     console.log(`${startTime}\n${display_f}\n${display_s}\n`)
-
+    
     //message.channel.send(output).then(update => { update.delete(10000) });
     switch(input[0]) {
         case '1':
             message.delete();
-            homu.channels.get('621547605138341898').sendEmbed(biTime.post(boss, lvl, bsimg, stat));
+            message.channel.send(display_f)
             stat = 2;
         break;
         case '2':
             message.delete();
-            homu.channels.get('621547605138341898').sendEmbed(biTime.post(boss, lvl, bsimg, stat));
+            message.channel.send(display_s)
             stat = 1;
         break;
         case '3':
             message.delete();
-            message.channel.send(`Time left before 5:00 PM is ${biTime.display2()}\n${biTime.calc2()} in miliseconds`).then(update => { update.delete(10000) });
+            message.channel.send(`AWS: ${current}   Home: ${startTime}\nTime left before 5:00 PM is ${biTime.display2()}\n${biTime.calc2()} in miliseconds`).then(update => { update.delete(10000) });
         break;
         default:
             message.channel.send('wah???').then(update => { update.delete(10000) });
         break;
     }
 };
+
+function bsEmbed(name, level, image, status) {
+    console.log(`${name}   ${level}\n${image}\n${status}`)
+    const emb = new Discord.RichEmbed()
+       .setColor(0xcccc00)
+       .setTitle('Next Boss has been set up!')
+       .setAuthor('Boss Invasion Alert!', 'https://i.imgur.com/QrJKwNl.png', ' ')
+       if(parseInt(status) === 1) {
+        emb.setDescription(`**${name}**\nLevel ${level}\nGame Starts in 1 hour`)
+       } else {
+        emb.setDescription(`**${name}**\nLevel ${level}\nBoss Invasion starts now.`)
+       };
+       emb.setThumbnail('https://i.imgur.com/JzDnCGJ.png')
+       emb.setImage(image)
+       emb.setTimestamp()
+       emb.setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
+    return emb;
+}
 
 exports.help = {
 	name: 'test3'
