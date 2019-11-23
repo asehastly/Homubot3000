7fea46d4e42546a1a14011e1ddd1225780444da0
asehastly <asehastly@gmail.com>
2019-11-24 00:03:22 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,6 @@
 const Discord = module.require('discord.js');
 const moment = module.require('moment-timezone');
+const { con } = require('../config.js');
 const biTime = module.require('../global/bitimer.js');
 
 
@@ -18,13 +19,54 @@ exports.run = (homu, message, args) => {
     //message.channel.send(output).then(update => { update.delete(10000) });
     switch(input[0]) {
         case '1':
-            message.delete();
-            message.channel.send(display_f)
-            stat = 2;
+            let id = '621517685750235157'
+            con.query(`SELECT * FROM boss WHERE id = ${id}`, (err, row) => {
+                if(err) throw err;
+                if(row.length < 1) return console.log('Table Boss is empty');
+                boss = row[0].name;
+                lvl = row[0].level;
+                bsimg = row[0].image;
+                stat = row[0].stat;
+                pstat = row[0].pstat;
+                console.log(stat);
+                if(!stat) {
+                    console.log('*WARNING*\nDatbase is empty!');
+                } else if(parseInt(pstat) === 1) {
+                    console.log('Post already been activated');
+                    message.channel.send('**Boss Invasion Alert** is currently on queue.').then(selfDestroy => { selfDestroy.delete(7000) });
+                } else if(parseInt(stat) === 1) {
+                    //if Boss name exist
+                    message.delete();
+                    console.log(`Boss ${row[0].name} has been set!`);
+                    console.log(`Manual trigger engaged.\nPhase 1 will start in ${biTime.display1()}`);
+                    message.channel.send(`**Boss Invasion Alert** is now live.\nSetting up alerts in ${biTime.display1()} and ${biTime.display2()} repectively.`).then(selfDestroy => { selfDestroy.delete(7000) });
+                    setTimeout(delay1 => {
+                        console.log('Executing Phase 1 now.');
+                        con.query(`UPDATE boss SET pstat = '1' WHERE id = '${id}'`, err => {
+                            if(err) throw err;
+                        });
+                        homu.channels.get('634607472560308234').send("<@&621558338005630978> Get ready!!!");
+                        homu.channels.get('634607472560308234').sendEmbed(biTime.post(boss, lvl, bsimg, stat));
+                        stat = 2;
+                    }, biTime.calc1());
+                    setTimeout(delay2 => {
+                        console.log('Second timer execute');
+                        homu.channels.get('634607472560308234').send("<@&621558338005630978> It's Go time!");
+                        homu.channels.get('634607472560308234').sendEmbed(biTime.post(boss, lvl, bsimg, stat));
+                        console.log('prepairing to delete database entires');
+                        /* con.query("DELETE FROM boss", function (err, result) {
+                            if (err) throw err;
+                            console.log('Databse has been cleared');
+                        }); */
+                    }, biTime.calc2());
+                } else {
+                    console.log('No boss was set');
+                };
+            });
         break;
         case '2':
             message.delete();
-            message.channel.send(display_s)
+            homu.channels.get('621547605138341898').sendEmbed(biTime.post(boss, lvl, bsimg, stat));
             stat = 1;
         break;
         case '3':
