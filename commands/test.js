8534d47a2fad838b8e0201f1d736c6b7322a533e
asehastly <asehastly@gmail.com>
2019-12-04 23:56:38 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,98 +5,22 @@ const biTime = module.require('../global/bitimer.js');
 
 
 exports.run = (homu, message, args) => {
-    const input = args;
-    var startTime = biTime.startTime().ct;
-    var display_f = biTime.calc1();
-    var display_s = biTime.calc2();
-    var boss = 'bushi';
-    var lvl = 55;
-    var bsimg = 'https://i.imgur.com/tzxM9XE.png';
-    var stat = 1;
-    var current = moment();
-    console.log(`${startTime}\n${display_f}\n${display_s}\n`)
-    
-    //message.channel.send(output).then(update => { update.delete(10000) });
-    switch(input[0]) {
-        case '1':
-            let id = '621517685750235157'
-            con.query(`SELECT * FROM boss WHERE id = ${id}`, (err, row) => {
-                if(err) throw err;
-                if(row.length < 1) return console.log('Table Boss is empty');
-                boss = row[0].name;
-                lvl = row[0].level;
-                bsimg = row[0].image;
-                stat = row[0].stat;
-                pstat = row[0].pstat;
-                console.log(stat);
-                if(!stat) {
-                    console.log('*WARNING*\nDatbase is empty!');
-                } else if(parseInt(pstat) === 1) {
-                    console.log('Post already been activated');
-                    message.channel.send('**Boss Invasion Alert** is currently on queue.').then(selfDestroy => { selfDestroy.delete(7000) });
-                } else if(parseInt(stat) === 1) {
-                    //if Boss name exist
-                    message.delete();
-                    console.log(`Boss ${row[0].name} has been set!`);
-                    console.log(`Manual trigger engaged.\nPhase 1 will start in ${biTime.display1()}`);
-                    message.channel.send(`**Boss Invasion Alert** is now live.\nSetting up alerts in ${biTime.display1()} and ${biTime.display2()} repectively.`).then(selfDestroy => { selfDestroy.delete(7000) });
-                    setTimeout(delay1 => {
-                        console.log('Executing Phase 1 now.');
-                        con.query(`UPDATE boss SET pstat = '1' WHERE id = '${id}'`, err => {
-                            if(err) throw err;
-                        });
-                        homu.channels.get('634607472560308234').send("<@&621558338005630978> Get ready!!!");
-                        homu.channels.get('634607472560308234').sendEmbed(biTime.post(boss, lvl, bsimg, stat));
-                        stat = 2;
-                    }, biTime.calc1());
-                    setTimeout(delay2 => {
-                        console.log('Second timer execute');
-                        homu.channels.get('634607472560308234').send("<@&621558338005630978> It's Go time!");
-                        homu.channels.get('634607472560308234').sendEmbed(biTime.post(boss, lvl, bsimg, stat));
-                        console.log('prepairing to delete database entires');
-                        /* con.query("DELETE FROM boss", function (err, result) {
-                            if (err) throw err;
-                            console.log('Databse has been cleared');
-                        }); */
-                    }, biTime.calc2());
-                } else {
-                    console.log('No boss was set');
-                };
-            });
-        break;
-        case '2':
-            message.delete();
-            homu.channels.get('621547605138341898').sendEmbed(biTime.post(boss, lvl, bsimg, stat));
-            stat = 1;
-        break;
-        case '3':
-            message.delete();
-            message.channel.send(`AWS: ${current}   Home: ${startTime}\nTime left before 5:00 PM is ${biTime.display2()}\n${biTime.calc2()} in miliseconds`).then(update => { update.delete(10000) });
-        break;
-        default:
-            message.channel.send('wah???').then(update => { update.delete(10000) });
-        break;
-    }
-};
-
-function bsEmbed(name, level, image, status) {
-    console.log(`${name}   ${level}\n${image}\n${status}`)
+    message.delete();
     const emb = new Discord.RichEmbed()
        .setColor(0xcccc00)
-       .setTitle('Next Boss has been set up!')
-       .setAuthor('Boss Invasion Alert!', 'https://i.imgur.com/QrJKwNl.png', ' ')
-       if(parseInt(status) === 1) {
-        emb.setDescription(`**${name}**\nLevel ${level}\nGame Starts in 1 hour`)
-       } else {
-        emb.setDescription(`**${name}**\nLevel ${level}\nBoss Invasion starts now.`)
-       };
-       emb.setThumbnail('https://i.imgur.com/JzDnCGJ.png')
-       emb.setImage(image)
-       emb.setTimestamp()
-       emb.setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
-    return emb;
-}
+       .setTitle('Before using this commands, please read this:')
+       .setAuthor('King Homu Commands', 'https://i.imgur.com/SuxUzng.png', ' ')
+       .setDescription(`In order to use King Homu properly, you have to use the prefix **homu** at the beggining of every commands. this prefix is case sensitive and must be typed in **lowercase** at all times, or else the command will not work.\n__Remember__ **homu** not Homu, nor home.`)
+       .addField('__**Disclaimer**__:', `In-case something went wrong or you mistyped, please contact the fucker who made me, **Don't blame me**.`)
+       .addField('**whois**', "This command will display the information of a mentioned member.\n**Reminder**: You have to mention someone in order for this command to work.\n*Syntax:* `homu whois <@someone>`")
+       .addField('**register**', "Use this command to register your in-game information so that it will show up to the member's card.\n*Syntax:* `homu register <your in-game name> <your game id>`")
+       .addField('**showrank**', "This command will display the current ranking of today's boss invasion.\n*Syntax:* `homu showrank`")
+       .setTimestamp()
+       .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
 
+       message.channel.sendEmbed(emb);
+
+}
 exports.help = {
-	name: 'test3'
+	name: 'test'
 }
