e9706c3154a69362b7f6e49da1f59afa2d3a81f0
asehastly <asehastly@gmail.com>
2019-12-08 15:19:07 +0800

    Update setboss.js

--- commands/setboss.js
+++ commands/setboss.js
@@ -31,22 +31,26 @@ exports.run = (homu, message, args) => {
                         console.log('*WARNING*\nDatbase is empty!');
                     }else if(parseInt(pstat) === 1){
                         console.log('Post already been activated');
+						message.channel.send('**Boss Invasion Alert** is currently on queue.').then(selfDestroy => { selfDestroy.delete(7000) });
                     }else if(parseInt(stat) === 1) {
                         //if Boss name exist
+						message.delete();
                         console.log(`Boss ${row[0].name} has been set!`);
+						console.log(`Manual trigger engaged.\nPhase 1 will start in ${biconfig.display1()}`);
+						message.channel.send(`**Boss Invasion Alert** is now live.\nSetting up alerts in ${biconfig.display1()} and ${biconfig.display2()} repectively.`).then(selfDestroy => { selfDestroy.delete(7000) });
                         setTimeout(delay1 => {
-                            console.log('First timer execute');
+                            console.log('Executing Phase 1 now.');
                             con.query(`UPDATE boss SET pstat = '1' WHERE id = '${id}'`, err => {
                                 if(err) throw err;
                             });
                             homu.channels.get('621547605138341898').send("<@&621558338005630978> Get ready!!!");
-                            homu.channels.get('621547605138341898').sendEmbed(bsEmbed(boss, lvl, bsimg, stat));
+                            homu.channels.get('621547605138341898').sendEmbed(biconfig.post(boss, lvl, bsimg, stat));
                             stat = 2;
                         }, biconfig.calc1());
                         setTimeout(delay2 => {
                             console.log('Second timer execute');
                             homu.channels.get('621547605138341898').send("<@&621558338005630978> It's Go time!");
-                            homu.channels.get('621547605138341898').sendEmbed(bsEmbed(boss, lvl, bsimg, stat));
+                            homu.channels.get('621547605138341898').sendEmbed(biconfig.post(boss, lvl, bsimg, stat));
                             console.log('prepairing to delete database entires');
                             /* con.query("DELETE FROM boss", function (err, result) {
                                 if (err) throw err;
@@ -114,7 +118,7 @@ exports.run = (homu, message, args) => {
                             sql = `INSERT INTO boss (id, name, level, image, stat, pstat) VALUES ('${server}', '${name}', '${parseInt(lvl)}', '${bsimg}', '1', '0')`;
                         } else {
                             let boss = rows[0].name;
-                            sql = `UPDATE boss SET name = '${name}', level = '${parseInt(lvl)}', image = '${bsimg}', stat = '1' pstat = '0' WHERE id = '${server}'`;
+                            sql = `UPDATE boss SET name = '${name}', level = '${parseInt(lvl)}', image = '${bsimg}', stat = '1', pstat = '0' WHERE id = '${server}'`;
                         }
         
                         con.query(sql,console.log);
@@ -133,9 +137,11 @@ exports.run = (homu, message, args) => {
                         .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
                         switch (condition) {
                             case '1':
+								message.delete();
                                 message.channel.sendEmbed(bossEmb);
                             return;
                             default:
+								message.delete();
                                 message.channel.send('Boss record not found. please try again...');
                             return;
                         }
@@ -146,14 +152,34 @@ exports.run = (homu, message, args) => {
                     }) */
         
                 } else {
+					message.delete();
                     message.channel.send('Only Authorized personnel can use this command.\nCome back later, if you have permission...');// not vip
                 }
         
             } else {
+				message.delete();
                 message.channel.send('Boss Invasion is still on going.\nYou can not set the boss yet.');
             }
         break;
     }
+};
+
+function bsEmbed(name, level, image, status) {
+    console.log(`${name}   ${level}\n${image}\n${status}`)
+    const emb = new Discord.RichEmbed()
+       .setColor(0xcccc00)
+       .setTitle('Next Boss has been set up!')
+       .setAuthor('Boss Invasion Alert!', 'https://i.imgur.com/QrJKwNl.png', ' ')
+       if(parseInt(status) === 1) {
+        emb.setDescription(`**${name}**\nLevel ${level}\nGame Starts in 1 hour`)
+       } else {
+        emb.setDescription(`**${name}**\nLevel ${level}\nBoss Invasion starts now.`)
+       };
+       emb.setThumbnail('https://i.imgur.com/JzDnCGJ.png')
+       emb.setImage(image)
+       emb.setTimestamp()
+       emb.setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
+    return emb;
 };
 
 exports.help = {
