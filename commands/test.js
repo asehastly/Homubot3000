2bd6c9d653c1b3f6a3b08ed544e6c640b5c2f85e
asehastly <asehastly@gmail.com>
2019-12-08 15:19:19 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,26 +1,89 @@
 const Discord = module.require('discord.js');
 const moment = module.require('moment-timezone');
 const { con } = require('../config.js');
-const biTime = module.require('../global/bitimer.js');
 
 
 exports.run = (homu, message, args) => {
-    message.delete();
-    const emb = new Discord.RichEmbed()
-       .setColor(0xcccc00)
-       .setTitle('Before using this commands, please read this:')
-       .setAuthor('King Homu Commands', 'https://i.imgur.com/SuxUzng.png', ' ')
-       .setDescription(`In order to use King Homu properly, you have to use the prefix **homu** at the beggining of every commands. this prefix is case sensitive and must be typed in **lowercase** at all times, or else the command will not work.\n__Remember__ **homu** not Homu, nor home.`)
-       .addField('__**Disclaimer**__:', `In-case something went wrong or you mistyped, please contact the fucker who made me, **Don't blame me**.`)
-       .addField('**whois**', "This command will display the information of a mentioned member.\n**Reminder**: You have to mention someone in order for this command to work.\n*Syntax:* `homu whois <@someone>`")
-       .addField('**register**', "Use this command to register your in-game information so that it will show up to the member's card.\n*Syntax:* `homu register <your in-game name> <your game id>`")
-       .addField('**showrank**', "This command will display the current ranking of today's boss invasion.\n*Syntax:* `homu showrank`")
-       .setTimestamp()
-       .setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
+	
+	var a = args[0];
+	var tag, ign, gid,armada,pfp;
+	
+	console.log(args.length);
+	message.delete();
+	if(!tag && !ign && !gid) return message.channel.send("Please include atleast a in-game name and game ID.\n**Syntax**: `homu addmember <**mention** *if applicable*> <**in-game name**> <**in-game ID number**>").then(update => { update.delete(15000)});
+	if(a.startsWith("<")) {
+		if(args.length >= 5) {
+			tag = args[0];
+			ign = `${args[1]} ${args[2]}`;
+			gid = args[3];
+			if(!args[4]) {
+				armada = 'No armada';
+			} else {
+				armada = args[4];
+			}
+			pfp = args[5];
+			console.log('Option 1.1');
+		} else {
+			tag = args[0];
+			ign = args[1];
+			gid = args[2];
+			if(!args[3]) {
+				armada = 'No armada';
+			} else {
+				armada = args[3];
+			}
+			pfp = args[4];
+			console.log('Option 1.2');
+		}
+	} else {
+		if(args.length > 3) {
+			tag = 'No discord Account';
+			ign = `${args[0]} ${args[1]}`;
+			gid = args[2];
+			if(!args[3]) {
+				armada = 'No armada';
+			} else {
+				armada = args[3];
+			}
+			pfp = args[4];
+			console.log('Option 2.1');
+		} else {
+			tag = 'No discord Account';
+			ign = args[0];
+			gid = args[1];
+			if(!args[2]) {
+				armada = 'No armada';
+			} else {
+				armada = args[2];
+			}
+			pfp = args[3];
+			console.log('Option 2.2');
+		}
+	}
+	
+	con.query(`SELECT * FROM memlistdummy WHERE gid = '${gid}'`, (err, rows) => {
+		if(err) throw err;
+		
+		let sql;
+		if(rows.length < 1) {
+			sql = `INSERT INTO memlistdummy (id, name, gid, armada) VALUES ('${tag}', '${ign}', '${parseInt(gid)}', '${armada}')`;
+		} else {
+			let boss = rows[0].name;
+			sql = `UPDATE memlistdummy SET id = '${tag}', name = '${ign}', armada = '${armada}' WHERE gid = '${gid}'`;
+		}
+		con.query(sql);
+	});
+	
+	const emb = new Discord.RichEmbed()
+		.setColor(0x23272a)
+		.addField("Account Information",`Discord tag: ${tag}\nIn-game name: ${ign}\nIn-game ID: ${gid}\nArmada: ${gid}`, true)
+		.setTimestamp()
+		.setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
+	message.channel.sendEmbed(emb);
+	console.log(args);
+	//message.channel.send(args).then(update => { update.delete(15000)});
+};
 
-       message.channel.sendEmbed(emb);
-
-}
 exports.help = {
 	name: 'test'
 }
