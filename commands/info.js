4c811361face6004c3d7d9c2bbf8e8ee3ad2a433
asehastly <asehastly@gmail.com>
2019-12-09 01:44:04 +0800

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,23 +1,30 @@
-module.exports = {
-	name: 'info',
-	description: 'something that involves the h!info command',
-	execute(msg, args) {
-        //code here
-        switch(args[1]){
-            case 'ver':
-                msg.channel.sendMessage('Version 1.x.x')
-            break;
-            case 'time':
-                var moment = require('moment')
-                var startDate = moment()
-                var endDate = moment('20:00:00', 'HH:mm:ss')
-                var calcTime = endDate.diff(startDate, 'miliseconds')
+const Discord = module.require('discord.js');
+const valks = require('../global/valks.js');
+const { con } = require('../config.js');
 
 
-                msg.channel.sendMessage(`the time difference between ${startDate.format('hh:mm:ss')} and ${endDate.format('hh:mm:ss')} is\n${calcTime} in miliseconds`)
-            break;
-            default:
-                msg.channel.sendMessage('WHAT?!')
-        }
-	},
+exports.run = (homu, message, args) => {
+    console.log(`------------------------------------------------\nUser has typed |${args}|\n------------------------------------------------`);
+    message.delete();
+    switch(args[0]) {
+        case undefined:
+            message.channel.send('Please tell me what to do... I am not a mind reader, you know...').then(update => { update.delete(7000)});
+        break;
+        case "uptime":
+            let totalSeconds = (homu.uptime / 1000);
+            let days = Math.floor(totalSeconds / 86400);
+            let hours = Math.floor(totalSeconds / 3600);
+            totalSeconds %= 3600;
+            let minutes = Math.floor(totalSeconds / 60);
+            let seconds = totalSeconds % 60;
+
+            message.channel.send(`I am running at\n${days} days\n${hours} hrs.\n${minutes} min.\n${seconds} sec.`).then(update => { update.delete(15000)});
+        break;
+        default: message.channel.send("I have no idea what you've just said... can you clarify that?").then(update => { update.delete(7000)});
+    }
+	
 };
+
+exports.help = {
+	name: 'test'
+}
