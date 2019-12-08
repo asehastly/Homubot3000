f02a41e298184fe0d39f45143f4cede897b482ba
asehastly <asehastly@gmail.com>
2019-12-08 20:38:13 +0800

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,87 +1,53 @@
 const Discord = module.require('discord.js');
-const moment = module.require('moment-timezone');
+const valks = require('../global/valks.js');
 const { con } = require('../config.js');
 
 
 exports.run = (homu, message, args) => {
 	
-	var a = args[0];
-	var tag, ign, gid,armada,pfp;
-	
-	console.log(args.length);
-	message.delete();
-	if(!tag && !ign && !gid) return message.channel.send("Please include atleast a in-game name and game ID.\n**Syntax**: `homu addmember <**mention** *if applicable*> <**in-game name**> <**in-game ID number**>").then(update => { update.delete(15000)});
-	if(a.startsWith("<")) {
-		if(args.length >= 5) {
-			tag = args[0];
-			ign = `${args[1]} ${args[2]}`;
-			gid = args[3];
-			if(!args[4]) {
-				armada = 'No armada';
-			} else {
-				armada = args[4];
-			}
-			pfp = args[5];
-			console.log('Option 1.1');
-		} else {
-			tag = args[0];
-			ign = args[1];
-			gid = args[2];
-			if(!args[3]) {
-				armada = 'No armada';
-			} else {
-				armada = args[3];
-			}
-			pfp = args[4];
-			console.log('Option 1.2');
-		}
-	} else {
-		if(args.length > 3) {
-			tag = 'No discord Account';
-			ign = `${args[0]} ${args[1]}`;
-			gid = args[2];
-			if(!args[3]) {
-				armada = 'No armada';
-			} else {
-				armada = args[3];
-			}
-			pfp = args[4];
-			console.log('Option 2.1');
-		} else {
-			tag = 'No discord Account';
-			ign = args[0];
-			gid = args[1];
-			if(!args[2]) {
-				armada = 'No armada';
-			} else {
-				armada = args[2];
-			}
-			pfp = args[3];
-			console.log('Option 2.2');
-		}
+	var valkqueue;
+	switch(args[0].toLowerCase()) {
+		case 'theresa':
+			valkqueue = 'Teri';
+		break;
+		case 'fu':
+			valkqueue = 'Fuka';
+		break;
+		case 'fuhua':
+			valkqueue = 'Fuka';
+		break;
+		case 'liliya':
+			valkqueue = 'Lily';
+		break;
+		case 'rozaliya':
+			valkqueue = 'Roza';
+		break;
+		default:
+			valkqueue = args[0];
 	}
-	
-	con.query(`SELECT * FROM memlistdummy WHERE gid = '${gid}'`, (err, rows) => {
-		if(err) throw err;
-		
-		let sql;
-		if(rows.length < 1) {
-			sql = `INSERT INTO memlistdummy (id, name, gid, armada) VALUES ('${tag}', '${ign}', '${parseInt(gid)}', '${armada}')`;
-		} else {
-			let boss = rows[0].name;
-			sql = `UPDATE memlistdummy SET id = '${tag}', name = '${ign}', armada = '${armada}' WHERE gid = '${gid}'`;
+	//message.delete();
+	for(let i = 0; i < valks.length; i++) {
+		//console.log(`${valks[i].valkName} | ${valks[i].vCode}`);
+		if(valkqueue.toUpperCase() === valks[i].vCode) {
+			let pfp = valks[i].pfp;
+			const emb = new Discord.RichEmbed()
+				.setColor('	#23272a')
+				.setAuthor('Valyrie Information', 'https://i.imgur.com/5ejjwD3.png', valks[i].url)
+				.setTitle(valks[i].valkName)
+				.setThumbnail(pfp[Math.floor(Math.random()*pfp.length)])
+				.addField('Weapon type', valks[i].weap)
+				.addField('Age', valks[i].Age, true)
+				.addField('Birthday', valks[i].DOB, true)
+				.addField('Height', valks[i].Height, true)
+				.addField('Weight', valks[i].Weight, true)
+				.addField('Measurements', valks[i].Measurements)
+				.addField('Bio', valks[i].vBio)
+			message.channel.sendEmbed(emb);
+			console.log(`------------------------------------------------\nUser has selected **${valks[i].vCode}**`);
+			console.log(`${pfp[Math.floor(Math.random()*pfp.length)]}\n------------------------------------------------`);
 		}
-		con.query(sql);
-	});
+	};
 	
-	const emb = new Discord.RichEmbed()
-		.setColor(0x23272a)
-		.addField("Account Information",`Discord tag: ${tag}\nIn-game name: ${ign}\nIn-game ID: ${gid}\nArmada: ${gid}`, true)
-		.setTimestamp()
-		.setFooter('Diamond Club Armada', 'https://i.imgur.com/FpIimN1.png');
-	message.channel.sendEmbed(emb);
-	console.log(args);
-	//message.channel.send(args).then(update => { update.delete(15000)});
 };
 
 exports.help = {
