deb1957764a93fbb941012c3b4fba417a1af6ddd
asehastly <asehastly@gmail.com>
2019-12-12 01:29:04 +0800

    Create arrays.js

---
+++ global/arrays.js
@@ -0,0 +1,19 @@
+
+module.exports =  [
+    {vCode:["KIANA"], Json:"valk01"},
+    {vCode:["MEI", "RAIDEN"], Json:"valk02"},
+    {vCode:["BRONYA"], Json:"valk03"},
+    {vCode:["HIMEKO"], Json:"valk04"},
+    {vCode:["SAKURA"], Json:"valk05"},
+    {vCode:["TERI", "THERESA", "TERIRI"], Json:"valk06"},
+    {vCode:["FUKA", "FU", "FUHUA"], Json:"valk07"},
+    {vCode:["KALLEN"], Json:"valk08"},
+    {vCode:["RITA"], Json:"valk09"},
+    {vCode:["LILY", "LILIYA"], Json:"valk10"},
+    {vCode:["ROZA", "ROZALIYA"], Json:"valk11"},
+    {vCode:["SEELE"], Json:"valk12"}
+];
+       
+
+
+
