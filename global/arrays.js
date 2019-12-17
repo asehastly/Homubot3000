93efb30269e1acfd0e802068f5441d963ac5bad0
asehastly <asehastly@gmail.com>
2019-12-17 10:31:20 +0800

    Update arrays.js

--- global/arrays.js
+++ global/arrays.js
@@ -4,14 +4,14 @@ module.exports =  [
     {vCode:["MEI", "RAIDEN"], Json:"valk02", sCode:["CI", "VB", "VBSF", "SD", "SDHN", "LE", "LEAC"]},
     {vCode:["BRONYA", "ZAYCHIK"], Json:"valk03", sCode:["VC", "VCBR", "SS", "YA", "YADK", "DB", "WD", "WDMH", "BN", "HOR"]},
     {vCode:["HIMEKO", "MURATA"], Json:"valk04", sCode:["BS", "VT", "SF", "SFSP", "BR", "AK", "AKFC", "VK"]},
-    {vCode:["SAKURA"], Json:"valk05", sCode:["MIKO", "MIKOSL", "MIKOSS", "FS", "MEME"]},
-    {vCode:["TERI", "THERESA", "TERIRI"], Json:"valk06", sCode:["VP", "VPP", "VPOF", "VPSS", "VPSHC", "RONDO", "RONDOLV", "RONDOMT", "LUNA", "LUNARB", "VE", "VEWT", "VETP", "CH"]},
+    {vCode:["SAKURA", "YAE"], Json:"valk05", sCode:["MIKO", "MIKOSL", "MIKOSS", "FS", "MEME"]},
+    {vCode:["TERI", "THERESA", "TERIRI", "APOCALYPSE"], Json:"valk06", sCode:["VP", "VPP", "VPOF", "VPSS", "VPSHC", "RONDO", "RONDOLV", "RONDOMT", "LUNA", "LUNARB", "VE", "VEWT", "VETP", "CH"]},
     {vCode:["FUKA", "FU", "FUHUA"], Json:"valk07", sCode:["VA", "PX", "PXFS", "NS", "NSRN", "NSST", "SK"]},
-    {vCode:["KALLEN"], Json:"valk08", sCode:["IR", "SJ", "6S"]},
-    {vCode:["RITA"], Json:"valk09", sCode:["UR", "URDC", "PI", "PIISS", "AKA", "AKAMC"]},
-    {vCode:["LILY", "LILIYA"], Json:"valk10", sCode:["BB"]},
-    {vCode:["ROZA", "ROZALIYA"], Json:"valk11", sCode:["MC"]},
-    {vCode:["SEELE"], Json:"valk12", sCode:["SP", "SPAM", "SPPL", "SN"]}
+    {vCode:["KALLEN", "KASLANA"], Json:"valk08", sCode:["IR", "SJ", "6S"]},
+    {vCode:["RITA", "ROSSWEISSE"], Json:"valk09", sCode:["UR", "URDC", "PI", "PIISS", "AKA", "AKAMC"]},
+    {vCode:["LILY", "LILIYA", "OLENYEVA"], Json:"valk10", sCode:["BB"]},
+    {vCode:["ROZA", "ROZALIYA", "OLENYEVA"], Json:"valk11", sCode:["MC"]},
+    {vCode:["SEELE", "VOLLEREI"], Json:"valk12", sCode:["SP", "SPAM", "SPPL", "SN"]}
 ];
        
 
