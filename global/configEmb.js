af78fe1318902295226907561f32608b3ded834f
asehastly <asehastly@gmail.com>
2019-12-26 01:28:52 +0800

    Update configEmb.js

--- global/configEmb.js
+++ global/configEmb.js
@@ -2,38 +2,54 @@ const Discord = require('discord.js');
 const homu = new Discord.Client();
 const valks = require('./arrays.js');
 const vJson = require('../json/valks.json');
-const { emojiDB } = require('./config.js');
+const { emojiDB, icon } = require('./config.js');
 
 module.exports = {
-    matchfound: async (json) => {
-		//console.log(valkfound.emoji);
-        let valkfound = vJson[json];
-        //console.log(valkfound);
-        let pfp = valkfound.pfp;
-        let bSuit = await emojiDB(valkfound, 3);
-        let cSuit = await emojiDB(valkfound, 4);
-		const embTrue = new Discord.RichEmbed()
-		.setColor('	#23272a')
-			.setAuthor('Valyrie Information', 'https://i.imgur.com/5ejjwD3.png', valkfound.url)
-			.setTitle(valkfound.valkName)
-			.setDescription(`**Weapon type**:${valkfound.weap}\n**Age**:${valkfound.Age}\t\t\t\t**Birthday**:${valkfound.DOB}\n**Measurements**:${valkfound.Measurements}\n**Height**: ${valkfound.Height}\t\t\t**Weight**:${valkfound.Weight}`)
+	foundEMB: async function (json) {
+		let valk = vJson[json];
+		let pfp = valk.pfp;
+		console.log(valk.vCode)
+		let bSuit = await emojiDB(valk.vCode[0], 3);
+		let cSuit = await emojiDB(valk.vCode[0], 4);
+		const trueEMB = new Discord.RichEmbed()
+			.setColor(valk.color)
+			.setAuthor('Valkyrie Information', 'https://i.imgur.com/5ejjwD3.png', valk.url)
+			.setTitle(valk.valkName)
+			.setDescription(`**Weapon type**: ${valk.weap}\n**Age**: ${valk.Age} **Birthday**: ${valk.DOB}\n**Measurement**: ${valk.Measurements}\n**Height**: ${valk.Height} **Weight**: ${valk.Weight}`)
 			.setThumbnail(pfp[Math.floor(Math.random()*pfp.length)])
-            .addField('Battlesuit', `${bSuit.map(r => `${r}`).join("\n")}`, true)
-			.addField('Outfits', `${cSuit.map(r => `${r}`).join("\n")}`, true)
-			.addField('Bio', valkfound.vBio)
-			.setTimestamp()
-            .setFooter('King Homu™ Archives', 'https://i.imgur.com/SuxUzng.png');
-		return embTrue;
-    },
-    notFound: (input) => {
-		let concat = input.join(" ");
-		const embFalse = new Discord.RichEmbed()
-		.setColor('	#23272a')
-			.setAuthor('Valyrie Information', 'https://i.imgur.com/5ejjwD3.png')
-			.setTitle(`${concat} was not on the database`)
-			.setDescription("Are you sure you are looking for a **playable** valkyrie?")
-			.setTimestamp()
-			.setFooter('King Homu™ Archives', 'https://i.imgur.com/SuxUzng.png');
-		return embFalse;
-    },
+			.addField('Battlesuit', bSuit.map(b => `${b.cEmoji} ${b.vEmoji} ${b.name}`).join('\n'), true)
+			.addField('Outfit', cSuit.map(c => `${c.cEmoji} ${c.vEmoji} ${c.name}`).join('\n'), true)
+			.addField('Bio', valk.vBio)
+			.setFooter('King Homu™ Archives', 'https://i.imgur.com/SuxUzng.png')
+		return trueEMB;
+	},
+	nFoundEMB: async function (arg) {
+		const noValkEMB = new Discord.RichEmbed()
+				.setColor('B33A3A')
+				.setAuthor('Valkyrie Information Profile', 'https://i.imgur.com/5ejjwD3.png')
+				.setTitle(`${arg.join(' ')} isn't a playable valkyrie. try again...`)
+				.setFooter('King Homu™ Archives', 'https://i.imgur.com/SuxUzng.png')
+		return noValkEMB;
+	},
+	errEMB: function (errOut) {
+		console.log(errOut);
+		console.log(errOut.length)
+		const noValkEMB = new Discord.RichEmbed()
+				.setColor(errOut.color)
+				.setAuthor(errOut.Auth, errOut.AuthIcon)
+				.setTitle(errOut.title)
+				.setFooter(errOut.foot, errOut.footIcon)
+		return noValkEMB;
+	},
+	dupEMB: function(x) {
+		const dupEMB = new Discord.RichEmbed()
+				.setColor('#303030')
+				.setAuthor('Valkyrie Selection', icon.valk)
+				.setTitle('Which valkyrie do you wish to see?')
+				for(let i = 0; i < x.length; i++) {
+					dupEMB.addField(x[i].emoTag, x[i].name, true)
+				}
+				dupEMB.setFooter('React only to 1 emoji below', icon.timer)
+		return dupEMB;
+	}
 }
